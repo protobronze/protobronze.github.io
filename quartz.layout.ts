85ff4cc2b1ab7dc32d4beb8c709bb4938caafef4
protobronze <wbain32@gmail.com>
2026-02-21 20:55:15 -0500

    Quartz sync: Feb 21, 2026, 8:55 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -19,7 +19,6 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
