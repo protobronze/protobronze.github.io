dab762e0588e0484e2e24dc057f2d500a85a1e9d
protobronze <wbain32@gmail.com>
2026-02-28 11:05:43 -0500

    Quartz sync: Feb 28, 2026, 11:05 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -25,7 +25,7 @@ const config: QuartzConfig = {
       typography: {
         header: "MayoClinicSerif",
         body: "NotoSansMono",
-        code: "IBM Plex Mono",
+        code: "NotoSansMono",
       },
       colors: {
         lightMode: {
